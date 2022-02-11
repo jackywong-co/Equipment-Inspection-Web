929b8ddbd06315a832104f92466f4136f950c127
Jacky Wong <jackywong.co@outlook.com>
2022-02-11 23:57:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,3 @@
-// import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import Header from "./components/Header/Header"
-// import { useContext } from "react";
-// import AuthContext from "./services/auth.context";
-// import NotFound from "./pages/Page404";
-
-
-// function App() {
-//   const authCtx = useContext(AuthContext)
-//   const theme = createTheme();
-
-//   return (
-//     <div className="App">
-//       <ThemeProvider theme={theme}>
-//         <Header />
-//         <Routes>
-
-//           {!authCtx.isLoggedIn && (
-//             <Route path="login" element={<Login />} />
-//           )}
-
-//           {authCtx.isLoggedIn && (
-//             <Route path="/" element={<Dashboard />} />
-//           )}
-
-//           <Route path="*" element={<Navigate to="404" />} />
-//           <Route path="404" element={<NotFound />} />
-
-
-
-//         </Routes>
-//       </ThemeProvider>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 // routes
 import Router from './routes';
 // theme
@@ -49,8 +5,6 @@ import ThemeConfig from './theme';
 import GlobalStyles from './theme/globalStyles';
 // components
 import ScrollToTop from './components/ScrollToTop';
-// import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
-
 // ----------------------------------------------------------------------
 
 export default function App() {
@@ -58,7 +12,6 @@ export default function App() {
     <ThemeConfig>
       <ScrollToTop />
       <GlobalStyles />
-      {/* <BaseOptionChartStyle /> */}
       <Router />
     </ThemeConfig>
   );
